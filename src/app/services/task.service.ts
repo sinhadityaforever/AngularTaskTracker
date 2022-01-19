e40b7cfdb7938401f8e81b3aec1f22f56247f96f
Aditya Sinha <sinhadityaforever@gmail.com>
2022-01-19 12:40:30 +0530

    Created backbone

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { TASKS } from '../mock-tasks';
+import { Task } from '../Task';
+import { Observable, of } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  constructor() {}
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
